ec0e4ee248a0b80c614c586dcee9f260881f299f
Julien Rialland <julien.rialland@gmail.com>
2019-08-01 12:03:30 +0200

    param name

--- src/main/java/com/github/jrialland/ajpclient/impl/handlers/OutgoingFramesLogger.java
+++ src/main/java/com/github/jrialland/ajpclient/impl/handlers/OutgoingFramesLogger.java
@@ -62,9 +62,9 @@ public class OutgoingFramesLogger extends ChannelOutboundHandlerAdapter {
 	}
 
 	private static final String bytesToHex(final byte[] data) {
-    		char[] hexChars = new char[bytes.length * 2];
-    		for (int j = 0; j < bytes.length; j++) {
-        		int v = bytes[j] & 0xFF;
+    		char[] hexChars = new char[data.length * 2];
+    		for (int j = 0; j < data.length; j++) {
+        		int v = data[j] & 0xFF;
         		hexChars[j * 2] = HEX_ARRAY[v >>> 4];
         		hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
     		}
