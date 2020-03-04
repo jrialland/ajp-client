24f0573d3ce0d60175665421972adfda85421be8
Julien Rialland <julien.rialland@brittany-ferries.fr>
2020-03-04 14:45:35 +0100

    updated header

--- src/main/java/com/github/jrialland/ajpclient/impl/ForwardImpl.java
+++ src/main/java/com/github/jrialland/ajpclient/impl/ForwardImpl.java
@@ -1,5 +1,5 @@
-/* Copyright (c) 2014-2017 Julien Rialland <julien.rialland@gmail.com>
- *
+/* Copyright (c) 2014-2020 Julien Rialland <julien.rialland@gmail.com>
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -9,7 +9,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ * 
  */
 package com.github.jrialland.ajpclient.impl;
 
--- src/test/java/com/github/jrialland/ajpclient/TestForward.java
+++ src/test/java/com/github/jrialland/ajpclient/TestForward.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2017 Julien Rialland <julien.rialland@gmail.com>
+/* Copyright (c) 2014-2020 Julien Rialland <julien.rialland@gmail.com>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
